c946298dfa7bbd93060154c7e265707b224f348b
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-05 02:40:51 +0300

    Create route.ts

---
+++ app/.well-known/farcaster.json/route.ts
@@ -0,0 +1,12 @@
+// app/.well-known/farcaster.json/route.ts
+import { generateManifest } from "@base-org/minikit";
+import miniapp from "../../minikit.config";
+
+export const dynamic = "force-static";
+
+export async function GET() {
+  const manifest = await generateManifest(miniapp);
+  return new Response(JSON.stringify(manifest), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
