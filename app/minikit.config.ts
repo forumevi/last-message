3f59ee9874704e7c0fe65c314a5be46ccaf8de35
Reddawn <35641620+forumevi@users.noreply.github.com>
2025-10-05 03:02:30 +0300

    Update minikit.config.ts

--- app/minikit.config.ts
+++ app/minikit.config.ts
@@ -4,9 +4,9 @@ import type { MiniAppManifest } from "@base-org/minikit";
 const miniapp: MiniAppManifest = {
   name: "The Last Message",
   description: "Everyone writes one sentence. Together, we write humanity’s last message.",
-  url: "https://last-message-lyart.vercel.app", // ← kendi URL'ni yaz
-  icon: "https://last-message-lyart.vercel.app/icon.png",
-  banner: "https://last-message-lyart.vercel.app/banner.png",
+  url: "https://last-message-git-main-forumevis-projects.vercel.app", // ← kendi URL'ni yaz
+  icon: "https://last-message-git-main-forumevis-projects.vercel.app",
+  banner: "https://last-message-git-main-forumevis-projects.vercel.app",
   author_fid: 0,
 };
 
